922c27286aa7a44802e167482ce6e8c5ed57c437
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-02-22 11:22:43 -0300

    Initial commit

---
+++ src/test/java/one/digitalinnovation/heroesapi/HeroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
