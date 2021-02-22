a66390627114118ea77cd525aba84354ab6a8c66
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-02-22 19:25:22 -0300

    feat: Heroes constants add

---
+++ src/main/java/one/digitalinnovation/heroesapi/constant/HeroesConstant.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.heroesapi.constant;
+
+public class HeroesConstant {
+
+  public static final String HEROES_ENDPOINT_LOCAL = "/heroes";
+  public static final String HEROES_TABLE_NAME = "HeroesCollection";
+  public static final String DYNAMO_ENDPOINT = "http://localhost:8000";
+  public static final String DYNAMO_REGION = "us-east-1";
+
+}
