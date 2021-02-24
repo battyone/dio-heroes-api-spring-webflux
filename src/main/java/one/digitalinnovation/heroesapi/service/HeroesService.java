39ccbfe4a739b8db7b09a951d9bb471e100c0461
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-02-24 16:14:47 -0300

    refactor: Heroes service repository autowired, save method rename

--- src/main/java/one/digitalinnovation/heroesapi/service/HeroesService.java
+++ src/main/java/one/digitalinnovation/heroesapi/service/HeroesService.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.heroesapi.service;
 
 import one.digitalinnovation.heroesapi.document.Heroes;
 import one.digitalinnovation.heroesapi.repository.HeroesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -9,11 +10,9 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class HeroesService {
-  private final HeroesRepository heroesRepository;
 
-  public HeroesService(HeroesRepository heroesRepository) {
-    this.heroesRepository = heroesRepository;
-  }
+  @Autowired
+  private HeroesRepository heroesRepository;
 
   public Flux<Heroes> findAllHeroes() {
     return Flux.fromIterable(this.heroesRepository.findAll());
@@ -24,7 +23,7 @@ public class HeroesService {
       .switchIfEmpty(Mono.error(new EmptyResultDataAccessException(1)));
   }
 
-  public Mono<Heroes> createHero(Heroes heroes) {
+  public Mono<Heroes> saveHero(Heroes heroes) {
     return Mono.justOrEmpty(this.heroesRepository.save(heroes));
   }
 
