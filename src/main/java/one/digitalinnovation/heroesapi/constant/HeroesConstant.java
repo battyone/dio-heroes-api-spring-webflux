f1ac3ec2ed8b4377d14d596d7c06f6f788d2e350
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-02-23 21:42:30 -0300

    fix: Heroes constants remove unused

--- src/main/java/one/digitalinnovation/heroesapi/constant/HeroesConstant.java
+++ src/main/java/one/digitalinnovation/heroesapi/constant/HeroesConstant.java
@@ -2,7 +2,6 @@ package one.digitalinnovation.heroesapi.constant;
 
 public class HeroesConstant {
 
-  public static final String HEROES_ENDPOINT_LOCAL = "/heroes";
   public static final String HEROES_TABLE_NAME = "HeroesCollection";
   public static final String DYNAMO_ENDPOINT = "http://localhost:8000";
   public static final String DYNAMO_REGION = "us-east-1";
